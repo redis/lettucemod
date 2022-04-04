d2c2c6216ff62a2a80d424583a8e7d1c133aaaf0
Julien Ruaux <julien@c64.arcillaux.lan>
2022-04-04 15:22:29 -0700

    Added query builder from JRediSearch. Resolves #15

---
+++ subprojects/lettucemod/src/main/java/com/redis/lettucemod/search/querybuilder/UnionNode.java
@@ -0,0 +1,11 @@
+package com.redis.lettucemod.search.querybuilder;
+
+/**
+ * Created by mnunberg on 2/23/18.
+ */
+public class UnionNode extends QueryNode {
+	@Override
+	protected String getJoinString() {
+		return "|";
+	}
+}
---
+++ subprojects/lettucemod/src/main/java/com/redis/lettucemod/search/querybuilder/Value.java
@@ -0,0 +1,12 @@
+package com.redis.lettucemod.search.querybuilder;
+
+/**
+ * Created by mnunberg on 2/23/18.
+ */
+public abstract class Value {
+	public boolean isCombinable() {
+		return false;
+	}
+
+	public abstract String toString();
+}
