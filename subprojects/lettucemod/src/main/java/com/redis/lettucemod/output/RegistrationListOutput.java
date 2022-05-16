dea3000c82da39550b488b1f363b3c47b6ba9d82
Julien Ruaux <julien@c64.arcillaux.lan>
2022-05-16 15:08:36 -0700

    test: Fixed RG.DUMPREGISTRATIONS tests

--- subprojects/lettucemod/src/main/java/com/redis/lettucemod/output/RegistrationListOutput.java
+++ subprojects/lettucemod/src/main/java/com/redis/lettucemod/output/RegistrationListOutput.java
@@ -15,6 +15,25 @@ import com.redis.lettucemod.gears.Registration;
 public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Registration>>
 		implements StreamingOutput<Registration> {
 
+	private static final String FIELD_NUM_TRIGGERED = "numTriggered";
+	private static final String FIELD_ID = "id";
+	private static final String FIELD_READER = "reader";
+	private static final String FIELD_DESC = "desc";
+	private static final String FIELD_REGISTRATION_DATA = "RegistrationData";
+	private static final String FIELD_MODE = "mode";
+	private static final String FIELD_LAST_ERROR = "lastError";
+	private static final String FIELD_ARGS = "args";
+	private static final String FIELD_STATUS = "status";
+	private static final String FIELD_PD = "PD";
+	private static final String FIELD_NUM_SUCCESS = "numSuccess";
+	private static final String FIELD_NUM_FAILURES = "numFailures";
+	private static final String FIELD_NUM_ABORTED = "numAborted";
+	private static final String FIELD_LAST_RUN_DURATION = "lastRunDurationMS";
+	private static final String FIELD_TOTAL_RUN_DURATION = "totalRunDurationMS";
+	private static final String FIELD_AVG_RUN_DURATION = "avgRunDurationMS";
+	private static final String FIELD_LAST_ESTIMATED_LAG = "lastEstimatedLagMS";
+	private static final String FIELD_AVG_ESTIMATED_LAG = "avgEstimatedLagMS";
+
 	private boolean initialized;
 	private Subscriber<Registration> subscriber;
 	private Registration registration;
@@ -32,40 +51,40 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 			field = decodeAscii(bytes);
 			return;
 		}
-		if (fieldEquals("id")) {
+		if (FIELD_ID.equals(field)) {
 			registration = new Registration();
 			registration.setId(decodeAscii(bytes));
 			field = null;
 			return;
 		}
-		if (fieldEquals("reader")) {
+		if (FIELD_READER.equals(field)) {
 			registration.setReader(decodeAscii(bytes));
 			field = null;
 			return;
 		}
-		if (fieldEquals("desc")) {
+		if (FIELD_DESC.equals(field)) {
 			registration.setDescription(decodeAscii(bytes));
 			field = null;
 			return;
 		}
-		if (fieldEquals("RegistrationData")) {
+		if (FIELD_REGISTRATION_DATA.equals(field)) {
 			registration.setData(new Registration.Data());
 			field = decodeAscii(bytes);
 			return;
 		}
 		if (registration != null) {
 			if (registration.getData() != null) {
-				if (fieldEquals("mode")) {
+				if (FIELD_MODE.equals(field)) {
 					registration.getData().setMode(decodeAscii(bytes));
 					field = null;
 					return;
 				}
-				if (fieldEquals("lastError")) {
+				if (FIELD_LAST_ERROR.equals(field)) {
 					registration.getData().setLastError(decodeAscii(bytes));
 					field = null;
 					return;
 				}
-				if (fieldEquals("args")) {
+				if (FIELD_ARGS.equals(field)) {
 					field = decodeAscii(bytes);
 					return;
 				}
@@ -74,71 +93,67 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 					field = null;
 					return;
 				}
-				if (fieldEquals("status")) {
+				if (FIELD_STATUS.equals(field)) {
 					registration.getData().setStatus(decodeAscii(bytes));
 					field = null;
 					return;
 				}
 			}
-			if (fieldEquals("PD")) {
+			if (FIELD_PD.equals(field)) {
 				registration.setPrivateData(decodeAscii(bytes));
 				field = null;
-				subscriber.onNext(output, registration);
-				registration = null;
-				// noinspection UnnecessaryReturnStatement
 				return;
 			}
+			subscriber.onNext(output, registration);
+			registration = null;
+			return;
 		}
-	}
-
-	private boolean fieldEquals(String name) {
-		return name.equals(field);
 	}
 
 	@Override
 	public void set(long integer) {
 		if (registration != null && registration.getData() != null) {
-			if (fieldEquals("numTriggered")) {
+			if (FIELD_NUM_TRIGGERED.equals(field)) {
 				registration.getData().setNumTriggered(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("numSuccess")) {
+			if (FIELD_NUM_SUCCESS.equals(field)) {
 				registration.getData().setNumSuccess(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("numFailures")) {
+			if (FIELD_NUM_FAILURES.equals(field)) {
 				registration.getData().setNumFailures(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("numAborted")) {
+			if (FIELD_NUM_ABORTED.equals(field)) {
 				registration.getData().setNumAborted(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("lastRunDurationMS")) {
+			if (FIELD_LAST_RUN_DURATION.equals(field)) {
 				registration.getData().setLastRunDurationMS(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("totalRunDurationMS")) {
+			if (FIELD_TOTAL_RUN_DURATION.equals(field)) {
 				registration.getData().setLastRunDurationMS(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("avgRunDurationMS")) {
+			if (FIELD_AVG_RUN_DURATION.equals(field)) {
 				registration.getData().setAvgRunDurationMS(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("lastEstimatedLagMS")) {
+			if (FIELD_LAST_ESTIMATED_LAG.equals(field)) {
 				registration.getData().setLastEstimatedLagMS(integer);
 				field = null;
 				return;
 			}
-			if (fieldEquals("avgEstimatedLagMS")) {
+			if (FIELD_AVG_ESTIMATED_LAG.equals(field)) {
 				registration.getData().setAvgEstimatedLagMS(integer);
 				field = null;
 				return;
@@ -146,6 +161,7 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 			if (registration.getData().getArgs() != null && registration.getData().getArgs().size() < argSize) {
 				registration.getData().getArgs().put(field, integer);
 				field = null;
+				return;
 			}
 		}
 	}
@@ -153,27 +169,27 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 	@Override
 	public void set(double number) {
 		if (registration != null && registration.getData() != null) {
-			if (fieldEquals("lastRunDurationMS")) {
+			if (FIELD_LAST_RUN_DURATION.equals(field)) {
 				registration.getData().setLastRunDurationMS(Math.round(number));
 				field = null;
 				return;
 			}
-			if (fieldEquals("totalRunDurationMS")) {
+			if (FIELD_TOTAL_RUN_DURATION.equals(field)) {
 				registration.getData().setLastRunDurationMS(Math.round(number));
 				field = null;
 				return;
 			}
-			if (fieldEquals("avgRunDurationMS")) {
+			if (FIELD_AVG_RUN_DURATION.equals(field)) {
 				registration.getData().setAvgRunDurationMS(Math.round(number));
 				field = null;
 				return;
 			}
-			if (fieldEquals("lastEstimatedLagMS")) {
+			if (FIELD_LAST_ESTIMATED_LAG.equals(field)) {
 				registration.getData().setLastEstimatedLagMS(Math.round(number));
 				field = null;
 				return;
 			}
-			if (fieldEquals("avgEstimatedLagMS")) {
+			if (FIELD_AVG_ESTIMATED_LAG.equals(field)) {
 				registration.getData().setAvgEstimatedLagMS(Math.round(number));
 				field = null;
 				return;
@@ -181,6 +197,7 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 			if (registration.getData().getArgs() != null && registration.getData().getArgs().size() < argSize) {
 				registration.getData().getArgs().put(field, number);
 				field = null;
+				return;
 			}
 		}
 	}
@@ -192,7 +209,7 @@ public class RegistrationListOutput<K, V> extends CommandOutput<K, V, List<Regis
 			initialized = true;
 			return;
 		}
-		if (fieldEquals("args") && registration != null && registration.getData() != null) {
+		if (FIELD_ARGS.equals(field) && registration != null && registration.getData() != null) {
 			argSize = count / 2;
 			registration.getData().setArgs(new HashMap<>(argSize));
 		}
