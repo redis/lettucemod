8ead9403edef88c9e3f5e1afa4f31f8cf16a561d
jruaux <jruaux@gmail.com>
2024-07-23 11:47:57 -0700

    test: Upgraded testcontainers redis

--- subprojects/spring-lettucemod/src/test/java/com/redis/spring/lettucemod/RedisEnterpriseIntegrationTests.java
+++ subprojects/spring-lettucemod/src/test/java/com/redis/spring/lettucemod/RedisEnterpriseIntegrationTests.java
@@ -14,22 +14,23 @@ import org.testcontainers.junit.jupiter.Container;
 
 import com.redis.enterprise.Database;
 import com.redis.enterprise.RedisModule;
+import com.redis.enterprise.testcontainers.RedisEnterpriseContainer;
 import com.redis.lettucemod.cluster.RedisModulesClusterClient;
-import com.redis.testcontainers.RedisEnterpriseContainer;
 
 @SpringBootTest(classes = TestApplication.class)
 @RunWith(SpringRunner.class)
 @EnabledOnOs(value = OS.LINUX)
 class RedisEnterpriseIntegrationTests {
 
+	private static final Database database = Database.builder().name("ModulesTests").memoryMB(110).ossCluster(true)
+			.modules(RedisModule.SEARCH, RedisModule.JSON, RedisModule.TIMESERIES).build();
+
 	@Autowired
 	private RedisModulesClusterClient client;
 
 	@Container
 	private static final RedisEnterpriseContainer container = new RedisEnterpriseContainer(
-			RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag("latest"))
-			.withDatabase(Database.builder().name("ModulesTests").memoryMB(110).ossCluster(true)
-					.modules(RedisModule.SEARCH, RedisModule.JSON, RedisModule.TIMESERIES).build());
+			RedisEnterpriseContainer.DEFAULT_IMAGE_NAME.withTag("latest")).withDatabase(database);
 
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
